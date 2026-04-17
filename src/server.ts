37aa3588c64bfd84d43022ec674a4226f8fa3d18
Dheeraj Thakur <dheerajthakur.dev@gmail.com>
2026-04-17 06:33:47 +0530

    Change server port to 3000

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import "./env.config.js"
 
 
-const port = process.env.PORT ?? 5000;
+const port = 3000;
 const DB_CONN_STR = process.env.ATLAS_CONN_STR || "mongodb://localhost:27017/edgeofesports";
 // "mongodb+srv://skybird:dheeraj2024@mongodb-cluster.0cfhzxs.mongodb.net/"
 mongoose.connect("mongodb+srv://skybird:dheeraj2024@mongodb-cluster.0cfhzxs.mongodb.net/edgeofesports?retryWrites=true&w=majority").then(()=>{
